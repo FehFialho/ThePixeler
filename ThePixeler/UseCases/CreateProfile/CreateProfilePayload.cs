185631032cc1a214999e39eb3228bb3de98ae1f7
BecaIanz <silveiraianzr@gmail.com>
2025-08-20 14:00:00 -0300

    New UpDate UseCases

--- ThePixeler/UseCases/CreateProfile/CreateProfilePayload.cs
+++ ThePixeler/UseCases/CreateProfile/CreateProfilePayload.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ThePixeler.UseCases.CreateProfile;
 
 public record CreateProfilePayload
 {
+    [Required]
+    [MinLength(8)]
+    [MaxLength(20)]
     public string UserName { get; set; }
+
+    [Required]
+    [MinLength(8)]
+    [MaxLength(8)]
     public string Password { get; set; }
+    
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     public string Image { get; set; }
 }
--- ThePixeler/UseCases/EditMember/EditMemberPayload.cs
+++ ThePixeler/UseCases/EditMember/EditMemberPayload.cs
@@ -1,9 +1,8 @@
 using ThePixeler.Models;
 namespace ThePixeler.UseCases.EditMember;
 
-public record EditMemberPayload
-{
-    public User Editor { get; set; }
-    public User Edited { get; set; }
-    public Role NewRole { get; set; }
-}
+public record EditMemberPayload(
+    User Editor,
+    User Edited,
+    Role NewRole
+);
