95b2208c7b1b95981bcae764df88ceaa131d845a
FehFialho <fernandaklechowicz@gmail.com>
2025-09-03 14:46:20 -0300

    Add Transients for UseCases and Services

--- ThePixeler/Services/ExtractJWTData/EFExtractJWTData.cs
+++ ThePixeler/Services/ExtractJWTData/EFExtractJWTData.cs
@@ -1,1 +1,21 @@
+using ThePixeler.Models;
+
 namespace ThePixeler.Services.ExtractJWTData;
+
+public class EFExtractJWTData(ThePixelerDbContext ctx) : IExtractJWTData
+{
+    public Task<Guid> GetUserGuid(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> GetUserSubscriptionID(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetUserUsername(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
